6dcd38b5cba6f1332d440dae8949d6f87c9bf7c5
Aldrich-ux <qian10282021@163.com>
2026-08-13 12:43:46 +0800

    BreadCrumb component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,11 +3,11 @@ import { useParams } from "react-router";
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 // Component
+import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
-
 // Image
 import NoImage from "../images/no_image.jpg";
 
@@ -16,11 +16,14 @@ const Movie = () => {
 
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
+
   console.log(movie);
 
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 };
