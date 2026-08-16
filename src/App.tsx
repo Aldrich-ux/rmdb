8ec385b2673c72145db703e0a20fe6677eb2d176
Aldrich-ux <qian10282021@163.com>
2026-08-16 10:14:28 +0800

    refactor to typescript on base files

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import NotFound from "./components/NotFound";
 // Styles
 import { GlobalStyle } from "./GlobalStyle";
 
-const App = () => (
+const App: React.FC = () => (
   <Router>
     <Header />
     <Routes>
